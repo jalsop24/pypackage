c4cdab012b501dd0083e57d62559a1fb2701a297
james <j.alsop99@hotmail.com>
2022-11-08 14:25:57 +0000

    Added request dependence

--- src/my_package/my_module.py
+++ src/my_package/my_module.py
@@ -1,3 +1,11 @@
 
+import requests
+
+TIME_URL = "http://worldtimeapi.org/api/timezone/Europe/London"
+
 def welcome():
     print("Hello World!")
+    
+    r = requests.get(TIME_URL)
+
+    print(r)
